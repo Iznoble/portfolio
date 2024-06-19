0b75f033110b42f951b13b34f14a9df1d7b2ca85
Iznoble <deniskusmitschev@outlook.de>
2024-06-19 12:52:47 +0200

    creadet components and landingPage

---
+++ src/app/main-content/landingpage/landingpage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+
+@Component({
+  selector: 'app-landingpage',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './landingpage.component.html',
+  styleUrl: './landingpage.component.scss'
+})
+export class LandingpageComponent {
+
+}
---
+++ src/app/main-content/main-content.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+
+@Component({
+  selector: 'app-main-content',
+  standalone: true,
+  imports: [LandingpageComponent],
+  templateUrl: './main-content.component.html',
+  styleUrl: './main-content.component.scss'
+})
+export class MainContentComponent {
+
+}
